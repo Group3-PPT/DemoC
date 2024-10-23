821c8d7a3c16145fc1a7522bb19010f8cc895ac6
kakashi01888 <hathat01888@gmail.com>
2024-10-23 22:31:22 +0800

    Update Group3.cpp
    Da Update lai

---
+++ Project/Project1/Group3.cpp
@@ -0,0 +1,9 @@
+float chia(float a, float b) {
+    if (b == 0) {
+        cout << "Khong the chia cho 0!" << endl;
+        return 0; // Hoặc bạn có thể xử lý trường hợp này theo cách khác
+    } else {
+        float thuong = a / b;
+        return thuong;
+    }
+}
